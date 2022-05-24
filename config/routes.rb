11229a55e57633a5b2f816d37fdc8778e039a3b1
keitoyokoi0628 <d51makbbcf@gmail.com>
2022-05-24 11:41:44 +0000

    TOPページをルートに設定、TOPページに新規投稿、投稿一覧のリンクを記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
   get 'lists/new'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
@@ -6,6 +7,6 @@ Rails.application.routes.draw do
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   patch 'lists/:id' => 'lists#update', as: 'update_list'
   delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
-  get '/top' => 'homes#top'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
